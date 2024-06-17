07651f7ca8350a476dca6400489c9d5c82924ebb
Zephirin Schmidt <zephirin@gmail.com>
2024-06-17 16:05:20 -0400

    Prep for Logging to stdout
    Added an operation in __init__.py that determines when the app should log to stdout.

--- ctwstatgrabber/config.py
+++ ctwstatgrabber/config.py
@@ -8,3 +8,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
